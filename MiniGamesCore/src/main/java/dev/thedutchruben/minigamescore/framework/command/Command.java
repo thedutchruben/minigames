95f16b86900c5c1859679ea1afd430b7fb06b286
Ruben de Roos <deroosr@gmail.com>
2019-09-14 11:57:13 +0200

    add command's

--- MiniGamesCore/src/main/java/dev/thedutchruben/minigamescore/framework/command/Command.java
+++ MiniGamesCore/src/main/java/dev/thedutchruben/minigamescore/framework/command/Command.java
@@ -1,4 +1,49 @@
 package dev.thedutchruben.minigamescore.framework.command;
 
-public class Command {
+import co.aurasphere.jyandex.dto.Language;
+import dev.thedutchruben.minigamescore.modules.language.LanguageModule;
+import lombok.Setter;
+import org.bukkit.command.CommandSender;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+
+public abstract class Command extends org.bukkit.command.Command {
+
+    private List<SubCommand> subCommands;
+    @Setter private boolean defaultList = true;
+    public Command(String command, String permission) {
+        super(command);
+        setPermission(permission);
+        this.subCommands = new ArrayList<>();
+    }
+
+    public void addSubCommand(SubCommand subCommand){
+        subCommands.add(subCommand);
+    }
+
+    @Override
+    public boolean execute(CommandSender sender, String commandLabel, String[] args) {
+        Optional<SubCommand> subCommand = subCommands.stream().filter(subCommand1 -> subCommand1.getCommand().equalsIgnoreCase(args[0])).findAny();
+        if(subCommand.isPresent()){
+            SubCommand subCommand1 = subCommand.get();
+            if(sender.hasPermission(subCommand1.getPermission())){
+                subCommand1.execute(sender);
+            }else{
+                //TODO NO PERMISSION MESSAGE
+                sender.sendMessage(LanguageModule.translate("Je hebt geen toestemming om dit uit te voeren!", Language.ENGLISH));
+            }
+        }else{
+            if(defaultList) {
+                //TODO send message with all commands
+            }else{
+                executeDefault(sender,args);
+            }
+        }
+        return false;
+    }
+
+    public abstract void executeDefault(CommandSender commandSender, String[] args);
 }
--- MiniGamesCore/src/main/java/dev/thedutchruben/minigamescore/framework/command/SubCommand.java
+++ MiniGamesCore/src/main/java/dev/thedutchruben/minigamescore/framework/command/SubCommand.java
@@ -1,4 +1,18 @@
 package dev.thedutchruben.minigamescore.framework.command;
 
-public class SubCommand {
+import lombok.Data;
+import org.bukkit.command.CommandSender;
+
+@Data
+public abstract class SubCommand {
+    private String command;
+    private String permission;
+    private String description = "No desciption";
+    public SubCommand(String command, String permission) {
+        this.command = command;
+        this.permission = permission;
+    }
+
+    public abstract void execute(CommandSender commandSender);
+
 }
