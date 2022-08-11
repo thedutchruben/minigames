a8009a741155762722473c83a85e09d161ded0e7
Ruben de Roos <deroosruben@gmail.com>
2022-08-11 23:47:44 +0200

    update workflow

---
+++ tntgames/src/main/java/nl/thedutchruben/minigames/tntgames/Minigames/Tntgames.java
@@ -0,0 +1,17 @@
+package nl.thedutchruben.minigames.tntgames.Minigames;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Tntgames extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
